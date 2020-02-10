04ae01ab722de9cb13f377502fc398b4fffdfe47
TAYLOR GEORGE <ATGEORGE@UTEXAS.EDU>
2020-02-10 10:18:38 -0600

    add twilio

--- routes/scheduleRide.js
+++ routes/scheduleRide.js
@@ -9,6 +9,10 @@ const MJ_API_KEY = process.env.MJ_API_KEY;
 const MJ_API_SECRET = process.env.MJ_API_SECRET;
 const mailjet = require('node-mailjet').connect(MJ_API_KEY, MJ_API_SECRET);
 
+//Twilio
+const twilio = require('twilio')(process.env.TWILIO_SID, process.env.TWILIO_AUTH_TOKEN);
+
+
 router.post('/api/newPickup', (req, res) => {
     let rideData = req.body.pickupData; 
 
@@ -19,50 +23,73 @@ router.post('/api/newPickup', (req, res) => {
         } else {
             console.log("Added a request!")
             if (data === "Pickup added!") {
-                // Sending Transaction email with details of ride request
-                const request = mailjet
-                    .post("send", {'version': 'v3.1'})
-                    .request({
-                        "Messages":[
-                            {
-                                "From": {
-                                    "Email": "jonathan.keane@galvanize.com",
-                                    "Name": "The Ritz-Carlton Residences"
-                                },
-                                "To": [
-                                    {
-                                        "Email": rideData.email,
-                                        "Name": rideData.name
-                                    }
-                                ],
-                                "TemplateID": 1208028,
-                                "TemplateLanguage": true,
-                                "Subject": "* Ritz-Carlton Residences: Confirmation Ride Request Information - Thank you!",
-                                "Variables": {
-                                        "TripDate": rideData.date,
-                                        "TripTime": rideData.startHoursMins,
-                                        "ArrivalTime": rideData.expectedArrivalTime,
-                                        "PickUpLocation": rideData.startAddress,
-                                        "DropOffLocation": rideData.endAddress,
-                                        "LuggageAmount": rideData.luggage,
-                                        "TotalTravelerAmount": rideData.travelers,
-                                        "RideShareBoolean": rideData.rideShare ? "Yes" : "No",
-                                        "appURL": "https://www.ritzcarlton.com/en/residences"
-                                    }
-                                }
-                            ]
+
+                //Sending Transaction text message with details of ride request
+                if (rideData.phoneNumber) {
+                    twilio.messages
+                        .create({
+                            body: "Ritz-Carlton Residences: You have a pickup scheduled for " +
+                                rideData.date + ".\n\n" +
+                                "You will be picked up at " + rideData.startAddress + ".\n\n" +
+                                "See the Ritz-Carlton Car Service app for more details.",
+                            from: '+18014365944',
+                            to: "+1" + rideData.phoneNumber
                         })
-                    request
-                    .then((result) => {
-                        console.log("Sent email");
-                        console.log("resolving with " + data);
-                        res.send(data);
-                    })
-                    .catch((err) => {
-                        console.log(err.statusCode)
-                        console.log("Sending error: " + err)
-                        res.send(err);
-                    })    
+                        .then(message => {
+                            console.log(message);
+                            res.send(data);
+                        })
+                        .catch(err => {
+                            console.log(err);
+                            res.send(data);
+                        });
+                }
+
+
+                //Sending Transaction email with details of ride request
+                // const request = mailjet
+                //     .post("send", {'version': 'v3.1'})
+                //     .request({
+                //         "Messages":[
+                //             {
+                //                 "From": {
+                //                     "Email": "jonathan.keane@galvanize.com",
+                //                     "Name": "The Ritz-Carlton Residences"
+                //                 },
+                //                 "To": [
+                //                     {
+                //                         "Email": rideData.email,
+                //                         "Name": rideData.name
+                //                     }
+                //                 ],
+                //                 "TemplateID": 1208028,
+                //                 "TemplateLanguage": true,
+                //                 "Subject": "* Ritz-Carlton Residences: Confirmation Ride Request Information - Thank you!",
+                //                 "Variables": {
+                //                         "TripDate": rideData.date,
+                //                         "TripTime": rideData.startHoursMins,
+                //                         "ArrivalTime": rideData.expectedArrivalTime,
+                //                         "PickUpLocation": rideData.startAddress,
+                //                         "DropOffLocation": rideData.endAddress,
+                //                         "LuggageAmount": rideData.luggage,
+                //                         "TotalTravelerAmount": rideData.travelers,
+                //                         "RideShareBoolean": rideData.rideShare ? "Yes" : "No",
+                //                         "appURL": "https://www.ritzcarlton.com/en/residences"
+                //                     }
+                //                 }
+                //             ]
+                //         })
+                //     request
+                //     .then((result) => {
+                //         console.log("Sent email");
+                //         console.log("resolving with " + data);
+                //         res.send(data);
+                //     })
+                //     .catch((err) => {
+                //         console.log(err.statusCode)
+                //         console.log("Sending error: " + err)
+                //         res.send(err);
+                //     })    
             } else {
                 console.log("No email");
                 console.log("Resolving with " + data);
